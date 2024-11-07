d553a9cd4df3d5afbd970c374e0226be9950520a
JonasLangeVonStocmeier <vonstocmeier@gmail.com>
2024-11-07 09:02:13 +0100

    Projektdateien hinzufügen.

---
+++ WpfContactBookMVVM/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfContactBookMVVM
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
